e595921ee0844b9fcfe809da2585b1df9c06c8fd
michael anikammadu <dil.anikamadu@gmail.com>
2025-07-05 19:05:59 +0100

    use folder name to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 declare PROJECT_NAME
+declare WORK_DIR="$(dirname "$(readlink -f $0)")"
+declare FOLDER_NAME="${WORK_DIR##*/}"
 if [ -z "$1" ]; then
-    echo "enter project name:"
-    read PROJECT_NAME
+    PROJECT_NAME="${FOLDER_NAME}"
 else
     PROJECT_NAME="${1}"
 fi
+
 echo "deploying to $PROJECT_NAME"
-gcloud run deploy $PROJECT_NAME --source . --region us-west1 --allow-unauthenticated
+gcloud run deploy $PROJECT_NAME --source . --region us-west1 --allow-unauthenticated --platform managed
 echo "deployed to $PROJECT_NAME"
 
 exit 0
